59b1dd0573555ed0f4f44e57fb76f8c77860a8c4
Your Name <you@example.com>
2023-05-29 23:48:43 -0400

    fondo azul

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "./semaforo.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [color, setColor] = useState("");
+  const [purple, setPurple] = useState(false);
+
+  const botonPurple = () => {
+    setPurple(!purple);
+  };
+
+  const alternado = () => {
+    if (color === "red") {
+      setColor("green");
+    } else if (color === "green") {
+      setColor("yellow");
+    } else {
+      setColor("red");
+    }
+  };
+
+  return (
+    <div>
+      <div className="cajanegra">
+        <div
+          onClick={() => setColor("red")}
+          className={"circuloRojo" + (color === "red" ? " luz" : "")}
+        ></div>
+        <div
+          onClick={() => setColor("yellow")}
+          className={"circuloAmarillo" + (color === "yellow" ? " luz" : "")}
+        ></div>
+        <div
+          onClick={() => setColor("green")}
+          className={"circuloVerde" + (color === "green" ? " luz" : "")}
+        ></div>
+        {purple && <div className={"circuloPurple"}></div>}
+      </div>
+      <div className="button-container">
+        <button onClick={botonPurple}>Color Extra</button>
+        <button onClick={alternado}>Intercambio de colores</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
